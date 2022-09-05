04b9413e5d3824aa7c8573ec022f0702cd089176
unknown <deividcuellocuevas@gmail.com>
2022-09-05 08:13:20 -0400

    My completed website hack.me

---
+++ README.md
@@ -0,0 +1,3 @@
+#Hack.me
+
+Página web estática de tipo blog realizada con HTML5, CSS3, Javascript.
---
+++ index.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector('.menu_icon');
+const bottomNavbar = document.querySelector('.bottom_navbar');
+
+
+menuIcon.addEventListener('click', () => bottomNavbar.classList.toggle('active'))
